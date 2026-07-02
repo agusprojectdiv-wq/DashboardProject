d2ec73dfea9fb08866bb9d46b935c3bfeca7454d
agusprojectdiv-wq <agus.projectdiv@gmail.com>
2026-07-02 11:39:45 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -19,3 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+window.db = db;
+
+console.log("Firebase berhasil terkoneksi");
